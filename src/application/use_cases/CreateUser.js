64bddab8841f976c541541b712a743524a3b09e0
Sqmyy <samy.heddadj@gmail.com>
2021-02-09 10:46:09 +0100

    assembling use_cases in one file

--- src/application/use_cases/CreateUser.js
+++
@@ -1,6 +0,0 @@
-const User = require('../../domain/User/User');
-
-module.exports = (firstName, lastName, email, {userRepository}) => {
-	const user = new User(null, firstName, lastName, email);
-	return userRepository.persist(user)
-};
--- src/application/use_cases/DeleteUser.js
+++
@@ -1,3 +0,0 @@
-module.exports = (userId, {userRepository}) => {
-	return userRepository.remove(userId);
-};
